7400f24f74a49575f0f9ae09e3cadeba4736ea1c
rladlad <rladlad@yahoo.com>
2025-08-07 20:58:58 +0300

    Added Serial Monitor Commands

--- RubikSelfSolver.ino
+++ RubikSelfSolver.ino
@@ -3,9 +3,13 @@
 using namespace RubikBot;
 
 Rubik* pCube {nullptr};
+String inputString = "";      // String to hold incoming data
+bool stringComplete = false; // Whether the string is complete
 
 //forward declarations
 void printFace(int face);
+void printCube(Rubik* cube);
+void clearSerialMonitor();
 
 void setup() {
   
@@ -17,19 +21,162 @@ void setup() {
   pCube = new Rubik();
   pCube->initialize();
 
-  char face[3][3];
 
-  printFace(1);
-  printFace(5);
+  inputString.reserve(50);
+
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (stringComplete) {
+    executeCommand(inputString);
+    inputString = "";
+    stringComplete = false;
+  }
+
 
 }
 
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n') {
+      stringComplete = true;
+    } else {
+      inputString += inChar;
+    }
+  }
+}
+
+void executeCommand(String cmd) {
+  cmd.trim(); // Remove whitespace and newline
+
+  if (cmd=="print"){
+    clearSerialMonitor();
+    printCube(pCube);
+  }
+  else if (cmd=="shuffle"){
+    pCube->shuffle(50);
+  }
+  else if (cmd=="init"){
+    pCube->initialize();
+  }
+  else if (cmd=="solve"){
+    clearSerialMonitor();
+    printCube(pCube);
+    pCube->solve();
+    printCube(pCube);
+  }
+  else if (cmd=="d"){	
+	  pCube->d();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="dp"){	
+    pCube->dp();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="l"){	
+    pCube->l();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="lp"){	
+    pCube->lp();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="f"){	
+    pCube->f();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="fp"){	
+    pCube->fp();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="r"){	
+    pCube->r();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="rp"){	
+    pCube->rp();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="b"){	
+    pCube->b();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="bp"){	
+    pCube->bp();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="u"){	
+    pCube->u();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="up"){	
+    pCube->up();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="d2"){	
+    pCube->d2();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="l2"){	
+    pCube->l2();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="f2"){	
+    pCube->f2();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="r2"){	
+    pCube->r2();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="b2"){	
+    pCube->b2();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else if (cmd=="u2"){	
+    pCube->u2();
+    clearSerialMonitor();
+    printCube(pCube);
+  }	
+  else{
+    Serial.println("Unknown Command");
+  }
+
+
+}
+
+
+
+
+
 //local functions
+
+void clearSerialMonitor() {
+  for (int i = 0; i < 50; i++) {
+    Serial.println();
+  }
+}
+
 void printFace(int face){
     char arr[3][3];
 
@@ -46,12 +193,66 @@ void printFace(int face){
 }
 
 void printCube(Rubik* cube){
-    char arr[3][3];
-    char blanks[3][3]{' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    char arr[9][12];
+    char temp[3][3];
 
-    cube->getFace(0, &arr[0][0]);
+    //initialize the array
+    for (int i=0; i<9; i++){
+      for (int j=0; j<12;j++)
+      {
+        arr[i][j]=' ';
+      }
+    }
 
-    //print top
-    
+    cube->getFace(0, &temp[0][0]);
+    for  (int i=3;i<6;i++){
+      for (int j=0;j<3;j++){
+        arr[i][j]=temp[i-3][j];
+      }
+    }
+
+    cube->getFace(1, &temp[0][0]);
+    for  (int i=3;i<6;i++){
+      for (int j=3;j<6;j++){
+        arr[i][j]=temp[i-3][j-3];
+      }
+    }
+
+    cube->getFace(2, &temp[0][0]);
+    for  (int i=3;i<6;i++){
+      for (int j=6;j<9;j++){
+        arr[i][j]=temp[i-3][j-6];
+      }
+    }
+
+    cube->getFace(3, &temp[0][0]);
+    for  (int i=3;i<6;i++){
+      for (int j=9;j<12;j++){
+        arr[i][j]=temp[i-3][j-9];
+      }
+    }
+
+    cube->getFace(4, &temp[0][0]);
+    for  (int i=0;i<3;i++){
+      for (int j=3;j<6;j++){
+        arr[i][j]=temp[i][j-3];
+      }
+    }
+
+    cube->getFace(5, &temp[0][0]);
+    for  (int i=6;i<9;i++){
+      for (int j=3;j<6;j++){
+        arr[i][j]=temp[i-6][j-3];
+      }
+    }
+
+    //print 
+    for (int i=0; i<9; i++){
+        for (int j=0; j<12 ; j++){
+            Serial.print(arr[i][j]);
+            Serial.print(' ');
+        }
+        Serial.println();
+      }
     
 }
